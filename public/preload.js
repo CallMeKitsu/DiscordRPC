23000c301174a30601e85f8622ce7272144d9b1f
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-11-06 17:49:08 +0100

    app fonctionnelle

--- public/preload.js
+++ public/preload.js
@@ -1,16 +1,60 @@
-const fs = require("fs")
+const fs = require("fs");
+const rpc = require("discord-rpc");
+const client = new rpc.Client({ transport: "ipc" });
 
-window.addEventListener('DOMContentLoaded', () => {
+let status = {
+  state: "",
+  details: "",
+  large_image: "",
+  large_text: "",
+  small_image: "",
+  small_text: "",
+  buttons: {},
+};
 
-  let statuses = []
-  for(var filename of fs.readdirSync('./status')) {
-    if(!filename.endsWith(".json")) return console.log("not a JSON file")
+window.addEventListener("DOMContentLoaded", () => {
+  let statuses = [];
+  for (var filename of fs.readdirSync("./status")) {
+    if (!filename.endsWith(".json")) return console.log("not a JSON file");
 
-    statuses.push(JSON.parse(fs.readFileSync(`./status/${filename}`)))
-
+    statuses.push(JSON.parse(fs.readFileSync(`./status/${filename}`)));
   }
 
-  const qr = new BroadcastChannel("data")
-  qr.postMessage(JSON.stringify(statuses))
-  
-})
+  const qr = new BroadcastChannel("data");
+  qr.postMessage(JSON.stringify(statuses));
+
+  document.querySelector("#run").addEventListener("click", (event) => {
+    let clientID = document.querySelector("#clientID").value;
+
+    status = {
+      state: document.querySelector("#state").value,
+      details: document.querySelector("#details").value,
+      large_image: document.querySelector("#large_image").value,
+      large_text: document.querySelector("#large_text").value,
+      small_image: document.querySelector("#small_image").value,
+      small_text: document.querySelector("#small_text").value,
+      buttons: {},
+    };
+
+    client.login({ clientId: clientID }).catch(console.error);
+  });
+});
+
+client.on("ready", () => {
+  client.request("SET_ACTIVITY", {
+    pid: process.pid,
+    activity: {
+      state: status.state,
+      details: status.details,
+
+      assets: {
+        large_image: status.large_image,
+        large_text: status.large_text,
+        small_image: status.small_image,
+        small_text: status.small_text,
+      },
+    },
+  });
+
+  alert("status loaded !");
+});
--- public/renderer.js
+++ public/renderer.js
@@ -1,27 +1,31 @@
 const bc = new BroadcastChannel("data");
-let ids = []
+let ids = [];
 
 bc.onmessage = (event) => {
-    let statuses = JSON.parse(event.data)
-    
-    for(let status of statuses) {
-        let name = status.name
-        let ID = status.appID
-        let state = status.state
-        let details = status.details
+  let statuses = JSON.parse(event.data);
 
-        let assets = JSON.parse(get(`https://discordapp.com/api/oauth2/applications/${ID}/assets`))
-        
-        let cover = assets.filter(asset => asset.name === status.large_image)[0].id
-        cover = `https://cdn.discordapp.com/app-assets/${ID}/${cover}.png`
-        let small = assets.filter(asset => asset.name === status.small_image)[0].id
-        small = `https://cdn.discordapp.com/app-assets/${ID}/${small}.png`
+  for (let status of statuses) {
+    let name = status.name;
+    let ID = status.appID;
+    let state = status.state;
+    let details = status.details;
 
-        let cover_text = assets.filter(asset => asset.name === status.large_text)
-        let small_text = assets.filter(asset => asset.name === status.small_text)
-    
-        let HTML = `
-        <div class="status">
+    let assets = JSON.parse(
+      get(`https://discordapp.com/api/oauth2/applications/${ID}/assets`)
+    );
+
+    let cover = assets.filter((asset) => asset.name === status.large_image)[0]
+      .id;
+    cover = `https://cdn.discordapp.com/app-assets/${ID}/${cover}.png`;
+    let small = assets.filter((asset) => asset.name === status.small_image)[0]
+      .id;
+    small = `https://cdn.discordapp.com/app-assets/${ID}/${small}.png`;
+
+    let cover_text = assets.filter((asset) => asset.name === status.large_text);
+    let small_text = assets.filter((asset) => asset.name === status.small_text);
+
+    let HTML = `
+        <div class="status" onclick='select(\`${JSON.stringify(status)}\`)'>
             <div class="images">
                 <img class="large" src="${cover}" title="${cover_text}">
                 <img class="small" src="${small}" title="${small_text}">
@@ -32,16 +36,27 @@ bc.onmessage = (event) => {
                 <span class="details">${details}</span>
             </div>
         </div>
-        `
+        `;
 
-        document.querySelector('#wrapper').innerHTML += HTML
-    
-    }
-}
+    document.querySelector("#wrapper").innerHTML += HTML;
+  }
+};
 
 function get(yourUrl) {
-    var Httpreq = new XMLHttpRequest()
-        Httpreq.open("GET", yourUrl, false)
-        Httpreq.send(null)
-    return Httpreq.responseText
+  var Httpreq = new XMLHttpRequest();
+  Httpreq.open("GET", yourUrl, false);
+  Httpreq.send(null);
+  return Httpreq.responseText;
+}
+
+function select(str_status) {
+  let status = JSON.parse(str_status);
+
+  document.querySelector("#clientID").value = status.appID;
+  document.querySelector("#state").value = status.state;
+  document.querySelector("#details").value = status.details;
+  document.querySelector("#large_image").value = status.large_image;
+  document.querySelector("#large_text").value = status.large_text;
+  document.querySelector("#small_image").value = status.small_image;
+  document.querySelector("#small_text").value = status.small_text;
 }
